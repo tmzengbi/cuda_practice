57fedd26699d31cf80dac3b484ebf3af20e4b74f
zengbi <714655944@qq.com>
2022-04-02 22:54:19 +0800

    task1 step1 global memory

--- work1/README.md
+++ work1/README.md
@@ -1,6 +1,12 @@
 使用下面一种或多种优化方法完成 CUDA 的矩阵向量乘法 $y = A \times x$, 其中 $A$ 是 $2^14 \times 2^14$ 的方阵, $x$ 是 $2^14$ 维向量。假设矩阵 $A$ 的元素为 $a_{ij}=i - 0.1 \times j + 1$, 向量 $x$ 的元素为 $b_i = \log\sqrt{i\times i - i + 2}$ 
 
 - 使用 global memory
+    本来想在 cpu 上提前计算好 $x$ 再传输到 gpu 上，考虑到传输延迟，决定直接在 gpu 上进行计算。
+    通过查询文档得到了 gpu 上单精度的数学库 https://docs.nvidia.com/cuda/cuda-math-api/group__CUDA__MATH__SINGLE.（单精度），html#group__CUDA__MATH__SINGLE  https://docs.nvidia.com/cuda/cuda-math-api/group__CUDA__MATH__DOUBLE.html#group__CUDA__MATH__DOUBLE（双精度）
+    其中单精度耗费时间 679ms，双精度耗费时间 3828ms
+
+    
+
 - 使用合并访存
 - 使用 constant memory 存放向量
 - 使用 shared memory 存放向量和矩阵
---
+++ work1/main.cu
@@ -0,0 +1,55 @@
+#include <cuda_runtime.h>
+#include <cstdio>
+
+const int N = 1 << 14;
+const int threadTot = 1024;
+
+/**
+ * @brief using global memory
+ */
+__global__ void calc(/*double *A, double *x, */double *y) {
+    // normal implementation
+    // for(int i = 0; i < N; ++ i)
+    //     for(int j = 0; j < N; ++ j)
+    //         y[i] += A[i][j] * x[j];
+#define A(i,j) (i - 0.1 * j + 1)
+#define x(i) logf(sqrtf(i * i - i + 2.0))
+    for(int i = threadIdx.x; i < N; i += blockDim.x)
+        for(int j = 0; j < N; ++ j)
+            // y[i] += A[idA] * x[j];
+            y[i] += A(i,j) * x(j);
+#undef A
+#undef x
+}
+
+#define cudaWork(work) \
+    do { \
+        work; \
+        cudaError_t err = cudaGetLastError(); \
+        if(err) { \
+            printf("error occur in line %d, %s\n", __LINE__, cudaGetErrorString(err)); \
+            exit(EXIT_FAILURE); \
+        } \
+    } while(0);
+
+int main() {
+    double *y, *res;
+    res = (double*) malloc(sizeof(double) * N);
+    cudaWork(cudaMalloc(&y, sizeof(double) * N));
+    cudaEvent_t start, stop;
+    float t = 0;
+    cudaWork(cudaEventCreate(&start));
+    cudaWork(cudaEventCreate(&stop));
+    cudaWork(cudaEventRecord(start));
+    calc<<<1,threadTot>>>(y);cudaWork();
+    cudaWork(cudaEventRecord(stop));
+    cudaWork(cudaEventSynchronize(stop));
+    cudaWork(cudaEventElapsedTime(&t, start, stop));
+    printf("elapsedTime %fms\n", t);
+    cudaWork(cudaMemcpy(res, y, sizeof(double) * N, cudaMemcpyDeviceToHost));
+    // for(int i = 0; i < N ; ++ i)
+    //     printf("%f\n", res[i]);
+    
+    cudaFree(y);
+    free(res);
+}
